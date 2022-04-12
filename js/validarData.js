81db7bdfa28bf0e02a28a78581ec9f64169c63a3
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-04-12 15:42:30 -0300

    Testando o .validity..valid do input

--- js/validarData.js
+++ js/validarData.js
@@ -11,6 +11,13 @@ export function valida (input){
     if (validaDataNascimento[tipoDeInput]) {
         validaDataNascimento[tipoDeInput](input)
     }
+
+    if (input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido')
+    } else{
+
+        input.parentElement.classList.add('input-container--invalido')
+    }
 }
 
 const validadores = {
